99337a7f71322ec40f45893e62349310a75eb674
ToanG1 <toandinh01675@gmail.com>
2024-04-15 23:21:41 +0700

    fix feed back

--- models/feedbacks.js
+++ models/feedbacks.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // const autoIncrement = require('mongoose-auto-increment');
 
 const feedbackSchema = new mongoose.Schema({
-    title: {
-        type: String
-    },
-    detail: {
-        type: String
-    },
-    created_at: {
-        type: Date,
-        default: Date.now()
-    },
-    emailId: {
-        type: String
-    },
-    product_id: {
-        type: String
-    },
-    role: {
-        type: String
-    },
-    feedback_id: {
-        type: Number
-    }
+  title: {
+    type: String,
+  },
+  detail: {
+    type: String,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  emailId: {
+    type: String,
+  },
+  product_id: {
+    type: String,
+  },
+  role: {
+    type: String,
+  },
+  feedback_id: {
+    type: Object,
+  },
 });
 // autoIncrement.initialize(mongoose.connection);
 // feedbackSchema.plugin(autoIncrement.plugin, 'feedbacks');
 
-const Feedbacks = mongoose.model('Feedbacks', feedbackSchema, 'feedbacks');
+const Feedbacks = mongoose.model("Feedbacks", feedbackSchema, "feedbacks");
 
 module.exports = {
-    Feedbacks
-}
+  Feedbacks,
+};
